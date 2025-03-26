1900742c8f7a188badbd9b13f70d145774c16745
Bartosz <s27929@pjwstk.edu.pl>
2025-03-26 16:41:47 +0100

    Dodano klasy Container, OverfillException, IHazardNotifier

---
+++ LoadingContainers/LoadingContainers/Exceptions/OverfillException.cs
@@ -0,0 +1,10 @@
+﻿namespace LoadingContainers.Exceptions;
+using System;
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message)
+    {
+        
+    }
+}
